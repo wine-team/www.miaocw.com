e48c44639e59bb1edc71f59a2e6db3c77f683db1
604942578 <604942578@qq.com>
2016-08-07 22:15:10 +0800

    首页的大整改

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -81,42 +81,51 @@
 	</div>
 	<div class="w">
 		<dl class="f_dl">
-			<dt>新手上路</dt>
-			<dd><a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">注册新用户</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">如何修改订单</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">如何订购</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">会员积分</a></dd>
+			<dt>购物指南</dt>
+			<dd>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">购物流程</a>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">会员介绍</a>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">免费注册</a>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">常见问题</a>
+			</dd>
 		</dl>
 		<dl class="f_dl">
 			<dt>支付方式</dt>
-			<dd><a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">在线方式</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">货到付款</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">优惠券支付</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">积分抵现</a></dd>
-		</dl>
-		<dl class="f_dl">
-			<dt>订购方式</dt>
-			<dd><a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">网站订购</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">400电话订购</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank" >微信订购</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">APP订购</a></dd>
+			<dd>
+			    <a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">网银支付</a>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">快捷支付</a>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">优惠券抵扣</a>
+			</dd>
 		</dl>
 		<dl class="f_dl">
 			<dt>配送与售后</dt>
-			<dd><a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">签收说明</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">快递物流</a>
+			<dd>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">签收验货</a>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">物流查询</a>
 				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">退/换货政策</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">隐私保护</a></dd>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">隐私保护</a>
+			</dd>
 		</dl>
 		<dl class="f_dl">
-			<dt>帮助中心</dt>
-			<dd><a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">忘记密码</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">常见问题</a>
-				<a rel="nofollow" href="javascript:jieshou();">在线客服</a>
-				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">联系我们</a></dd>
+			<dt>订购方式</dt>
+			<dd>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">网站订购</a>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank" >微信订购</a>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">APP订购</a>
+			    <a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">400电话订购</a>
+			</dd>
+		</dl>
+		<dl class="f_dl">
+			<dt>商家服务</dt>
+			<dd>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">商家入驻</a>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">培训中心</a>
+				<a rel="nofollow" href="javascript:jieshou();">广告服务</a>
+				<a rel="nofollow" href="<?php echo $this->config->help_url;?>" target="_blank">商家帮助</a>
+			</dd>
 		</dl>
 		<dl class="f_dl mr0">
-			<dt>APP下载</dt>
+			<dt>妙处网客户端</dt>
 			<dd><img src="" width="100" height="100"></dd>
 		</dl>
 		<div class="clear"></div>
@@ -126,24 +135,28 @@
 			<p>
 				<a target="_blank" href="<?php echo $this->config->main_base_url;?>" class="c9">首页</a>
 				<span class="vline">|</span>
-				<a target="_blank" rel="nofollow" href="<?php echo $this->config->help_url;?>" class="c9">正品,隐私保障</a>
+				<a target="_blank" rel="nofollow" href="<?php echo $this->config->help_url;?>" class="c9">关于我们</a>
 				<span class="vline">|</span>
-				<a target="_blank" href="<?php echo $this->config->help_url;?>" rel="nofollow" class="c9">公司简介</a>
+				<a target="_blank" href="<?php echo $this->config->help_url;?>" rel="nofollow" class="c9">联系我们</a>
 				<span class="vline">|</span>
-				<a rel="nofollow" target="_blank" href="<?php echo $this->config->help_url;?>" class="c9">帮助中心</a>
+				<a target="_blank" href="<?php echo $this->config->help_url;?>" rel="nofollow" class="c9">联系客服</a>
 				<span class="vline">|</span>
-				订购热线 <span>400-660-0606</span>
+				<a rel="nofollow" target="_blank" href="<?php echo $this->config->help_url;?>" class="c9">商家入驻</a>
+				<span class="vline">|</span>
+				订购热线 <span>888-8888-8888</span>
 			</p>
 			<p>Copyright © 2015 ICP经营性许可证:浙ICP备8888888号-2</p>
-			<p>公司：杭州小医仙网络科技有限公司,电话：888-888-8888</p>
+			<p>公司：杭州小医仙网络科技有限公司</p>
+			<!--  
 			<p class="mt10">
-				<a href="https://itunes.apple.com/us/app/qu-wang-qing-qu-shang-cheng/id934737326?mt=8" target="_blank" title="趣网商城iPhone,ipad版" rel="nofollow" class="ml5 mr5">
+				<a href="javascript:;" target="_blank" title="妙处网iPhone,ipad版" rel="nofollow" class="ml5 mr5">
 					<img src="passport/images/sf2.png" width="110" height="30">
 				</a>
-				<a href="http://www.qw.cc/app/mobile.apk" target="_blank" title="趣网商城Android版" rel="nofollow">
+				<a href="javascript:;" target="_blank" title="妙处网Android版" rel="nofollow">
 					<img src="passport/images/sf3.png" width="110" height="30">
 				</a>
 			</p>
+			-->
 		</div>
 	</div>
 </div>
