c3a332d240c28db8ace843413f18532bfc6ac26e
604942578 <604942578@qq.com>
2016-08-20 17:39:49 +0800

    产品详情

--- application/views/home/historylist.php
+++ application/views/home/historylist.php
@@ -1,6 +1,6 @@
 <?php if (count($history)>0 && !empty($goods) ) :?>
 <?php foreach ($goods->result() as $key=>$item):?>
-<?php $goodsImage = explode('|',$item->goods_img);?>
+<?php $goodsImage = array_filter(explode('|',$item->goods_img));?>
 <a title="<?php echo $item->goods_name;?>" class="hs_ra" target="_blank" href="">
 	<img width="70" height="70" src="<?php echo $this->config->show_image_thumb_url('mall',$goodsImage[0]);?>">
 	<p>¥<?php echo $item->promote_price;?></p>
