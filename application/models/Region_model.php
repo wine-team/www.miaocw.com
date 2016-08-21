49de14a8da15982e6da95a313751fb2ea19e6717
604942578 <604942578@qq.com>
2016-08-21 17:25:39 +0800

    产品详情

--- application/models/Region_model.php
+++ application/models/Region_model.php
@@ -17,4 +17,14 @@ class Region_model extends CI_Model
     	return $this->db->get();
     }
     
+    function children_of($parent_id)
+    {
+    	$this->db->where('parent_id', (int)$parent_id);
+    	$result = $this->db->get($this->table);
+    	if ($result->num_rows() > 0){
+    		return $result->result_array();
+    	}
+    	return array();
+    }
+    
 }
