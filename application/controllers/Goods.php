214a5752ecc4c82b2d43343f7bdf2e18f2300dd6
wenjoying <wenjoying@126.com>
2016-08-25 22:39:54 +0800

    关键字可以为空

--- application/controllers/Goods.php
+++ application/controllers/Goods.php
@@ -23,9 +23,9 @@ class Goods extends MW_Controller{
 	 */
 	public function search($pg = 1){
 		
-	    if(empty($this->input->get('keyword'))) {
-	        $this->alertJumpPre('请输入关键字');
-	    }
+// 	    if(empty($this->input->get('keyword'))) {
+// 	        $this->alertJumpPre('请输入关键字');
+// 	    }
 	    $page_num = 20;
 	    $num = ($pg-1)*$page_num;
 	    $searchTotal = $this->mall_goods_base->searchTotal($this->input->get());
