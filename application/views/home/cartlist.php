8a05dae37a1d2244b9b1e63a4e25ec3a5867bc93
604942578 <604942578@qq.com>
2016-09-23 07:52:03 +0800

    购物车

--- application/views/home/cartlist.php
+++ application/views/home/cartlist.php
@@ -2,16 +2,24 @@
 <ul class="acar">
 <?php $num=0;$total=0;?>
 <?php foreach ($cart_goods->result() as $key=>$item):?>
-     <?php $goodsImage = explode('|',$item->goods_img);?>
     <li>
 		<a href="javascript:;">
-			<img class="left" width="60" height="60" alt="妙处网" src="<?php echo $this->config->show_image_thumb_url('mall',$goodsImage[0]);?>" />
-			<p><?php echo $item->goods_name?></p>
-			<p class="red"><?php echo $item->promote_price;?><b class="c5"> X </b><?php echo $item->goods_num;?></p>
+			<img class="left" width="60" height="60" alt="妙处网" src="<?php echo $this->config->show_image_thumb_url('mall',strstr($item->goods_img,'|',true));?>" />
+			<p><?php echo $item->goods_name;?></p>
+			<p class="red">
+			    <?php if( !empty($item->promote_price) && !empty($item->promote_start_date) && !empty($item->promote_end_date) && ($item->promote_start_date<=time()) && ($item->promote_end_date>=time())):?>
+					<?php $total_price = $item->promote_price;?>
+				<?php else:?>
+					<?php $total_price = $item->shop_price;?>
+				<?php endif;?>
+				<?php echo $total_price;?>
+				<b class="c5"> X </b>
+				<?php echo $item->goods_num;?>
+			</p>
 		</a>
 	</li>
-	 <?php $num += $item->goods_num; ?>
-	 <?php $total += bcmul($item->goods_num, $item->promote_price,2);?>
+	 <?php $num += $item->goods_num;?>
+	 <?php $total += bcmul($item->goods_num, $total_price,2);?>
 <?php endforeach;?>
 </ul>
 <div class="ac_t alR clearfix">
