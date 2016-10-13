ef98bc652e52cbace59d1e69a289fb87794fe991
604942578 <604942578@qq.com>
2016-10-13 23:08:02 +0800

    config

---
+++ application/views/home/lingeries.php
@@ -0,0 +1,122 @@
+		<p class="lh35">&nbsp;</p>
+		<div class="h_tt over">
+			<em class="flor left">2F</em>
+			<h2 class="left">
+				<a target="_blank" href="<?php echo site_url('goods/search?category_id=25');?>" class="h_ha">情趣内衣</a>
+				<em class="gray pl5 f16">LINGERIES</em>
+			</h2>
+			<p class="right">
+				<a href="<?php echo site_url('goods/search?category_id=33');?>" target="_blank" class="gray">性感裙装</a>
+				<em class="vline">|</em>
+				<a href="<?php echo site_url('goods/search?category_id=34');?>" target="_blank" class="gray">三点激情</a>
+				<em class="vline">|</em>
+				<a href="<?php echo site_url('goods/search?category_id=35');?>" target="_blank" class="gray">情趣内裤</a>
+				<em class="vline">|</em>
+				<a href="<?php echo site_url('goods/search?category_id=36');?>" target="_blank" class="gray">连体网衣</a>
+				<em class="vline">|</em>
+				<a href="<?php echo site_url('goods/search?category_id=37');?>" target="_blank" class="gray">情趣丝袜</a>
+			    <em class="vline">|</em>
+			    <a href="<?php echo site_url('goods/search?category_id=38');?>" target="_blank" class="gray">男士内衣</a>
+			    <em class="vline">|</em>
+				<a href="<?php echo site_url('goods/search?category_id=25');?>" target="_blank" class="org" rel="nofollow">更多>></a>
+			</p>
+		</div>
+		<div class="h_fl">
+			<div class="h_cat">
+				<h3 class="mb5">飞机杯/增大</h3>
+				<a class="c_zon" href="<?php echo site_url('goods/search?keyword=电动型');?>" target="_blank">电动型</a>
+				<a href="<?php echo site_url('goods/search?keyword=手动型');?>" target="_blank">手动型</a>
+				<a href="<?php echo site_url('goods/search?keyword=免提式');?>" target="_blank">免提式</a>
+				<a href="<?php echo site_url('goods/search?keyword=阴交型');?>" class="c_zon" target="_blank">阴交型</a>
+				<a href="<?php echo site_url('goods/search?keyword=电动增大');?>" class="c_zon" target="_blank">电动增大</a>
+				<a href="<?php echo site_url('goods/search?keyword=两用型');?>" target="_blank">两用型</a>
+			</div>
+			<div class="h_cat">
+				<h3 class="mb5">真人倒模</h3>
+				<a href="<?php echo site_url('goods/search?keyword=两用型');?>"  class="c_zon"target="_blank">阴部</a>
+				<a href="<?php echo site_url('goods/search?keyword=两用型');?>" target="_blank">臀部</a>
+				<a href="<?php echo site_url('goods/search?keyword=两用型');?>" target="_blank" class="c_zon">AV女优</a>
+				<a href="<?php echo site_url('goods/search?keyword=两用型');?>" target="_blank" >真人呻吟</a>
+			</div>
+			<div class="h_cat">
+				<h3 class="mb5">其他</h3>
+				<a href="<?php echo site_url('goods/search?keyword=两用型');?>" class="c_zon" target="_blank">充气娃娃</a>
+				<a href="<?php echo site_url('goods/search?keyword=两用型');?>" target="_blank">实体娃娃</a>
+				<a href="<?php echo site_url('goods/search?keyword=两用型');?>" target="_blank">前列腺刺激</a>
+				<a href="<?php echo site_url('goods/search?keyword=两用型');?>" target="_blank">延时喷剂</a>
+			</div>
+			<a class="h_ff" target="_blank" href="<?php echo site_url('goods/femal');?>">
+				<img src="http://s.qw.cc/themes/v4/css/ft/f1.jpg" width="123" height="75" alt="男性用品">
+			</a>
+		</div>
+		<div class="h_fm">
+			<a href="<?php echo site_url('goods/femal');?>" target="_blank">
+				<img src="http://s.qw.cc/data/afficheimg/1451448053762084083.jpg" width="400" height="390">
+			</a>
+		</div>
+		<div class="h_fr">
+	        <a href="<?php echo site_url('goods/detail');?>" class="f_aa" target="_blank">
+	            <div class="f_am">
+	            	飞机杯式<em class="red">增大器</em>
+					<p>电动夹吸，4D通道</p>
+					<i>¥459</i>
+				</div>
+				<img src="http://s.qw.cc/data/afficheimg/1453446640724076722.jpg" width="160" height="170" class="ha_po" />
+		    </a>
+	        <a href="<?php echo site_url('goods/detail');?>" class="f_aa" target="_blank">
+	            <div class="f_am">
+	                                             国优<em class="red">名器</em>
+	                                             兽兽宝贝<p>私处仿真倒模</p>
+	                <i>¥199</i>
+	            </div>
+	            <img src = "http://s.qw.cc/data/afficheimg/1441781567790022284.jpg" width="160" height="170" class="ha_po"/>
+	        </a>
+	        <a href="<?php echo site_url('goods/detail');?>" class="f_aa" target="_blank">
+	            <div class="f_am">
+	                                             触控型<em class="red">真人发声</em>
+	            	飞机杯<p>配嫩模真人叫床声</p>
+	            	<i>¥299</i>
+	            </div>
+	            <img src = "http://s.qw.cc/data/afficheimg/1449233890346675557.jpg" width="160" height="170" class="ha_po" />
+	        </a>
+	        <a href="<?php echo site_url('goods/detail');?>" class="f_aa" target="_blank">
+	           <div class="f_am">
+	            	<em class="red">英国Revo</em>
+	            	前列腺按摩<p>360°旋转式按摩</p>
+	            	<i>¥1799</i>
+	           </div>
+	           <img src = "http://s.qw.cc/data/afficheimg/1456883466852655483.jpg" width="160" height="170" class="ha_po">
+	        </a>
+	    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
