186947e0f8dfc9e02cbf09350b3e96c1f8819032
604942578 <604942578@qq.com>
2016-10-15 15:29:06 +0800

    config

---
+++ application/models/mall_category_model.php
@@ -0,0 +1,45 @@
+<?php
+ 
+class Mall_category_model extends CI_Model{
+	
+	private $table = 'mall_category';
+	
+	/**
+	 *获取所有的分类
+	 */
+	public function getAllCategory(){
+	
+		$this->db->where('is_show',1);
+		$category = $this->db->get($this->table);
+		$firstCat = array();
+		foreach ($category->result() as $key=>$val) {
+			if (($val->parent_id==0) ) {
+				$firstCat[$key]['cat_id'] = $val->cat_id;
+				$firstCat[$key]['cat_name'] = $val->cat_name;
+				 //$firstCat[$key]['keyword'] = $val->keyword;
+			}
+		}
+		foreach ($firstCat as $key=>$item){
+			foreach ($category->result() as $ikey=>$jitem) {
+				if( $jitem->parent_id == $item['cat_id'] ){
+					$firstCat[$key]['childCat'][$ikey]['cat_id'] = $jitem->cat_id;
+					$firstCat[$key]['childCat'][$ikey]['cat_name'] = $jitem->cat_name;
+				}
+			}
+		}
+		return $firstCat;
+	}
+	
+	 /**
+	 * 获取下级分类ID
+	 * @param unknown $parent_id
+	 */
+	public function getChildCat($parent_id){
+		
+		$this->db->select('cat_name,cat_id');
+		$this->db->from($this->table);
+		$this->db->where('parent_id',$parent_id);
+		$this->db->where('is_show',1);
+	    return $this->db->get();
+	}
+}
---
+++ application/models/mall_keyword_model.php
@@ -0,0 +1,6 @@
+<?php
+class Mall_keyword_model extends CI_Model
+{
+	private $table = 'mall_keyword';
+    
+}
