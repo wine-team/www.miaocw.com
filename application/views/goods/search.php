9f0cc1c676fd73db92d66a8cb277bcc27fc5dd9d
604942578 <604942578@qq.com>
2016-10-23 20:21:32 +0800

    config

--- application/views/goods/search.php
+++ application/views/goods/search.php
@@ -66,8 +66,5 @@
 	    <?php echo $cms_block['foot_recommend_img'];?>
 	</div>
 	
-	<div class="fa_l clearfix mt35">
-		<?php echo $cms_block['foot_speed_key'];?>
-	</div>
 </div>
 <?php $this->load->view('layout/footer');?>
