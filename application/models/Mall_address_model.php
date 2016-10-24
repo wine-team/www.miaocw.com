94de4a68afa8410b40edd7a82a6e944230010441
604942578 <604942578@qq.com>
2016-10-25 07:19:10 +0800

    config

--- application/models/Mall_address_model.php
+++ application/models/Mall_address_model.php
@@ -7,13 +7,14 @@ class Mall_address_model extends CI_Model
      * 发现地址
      * @param unknown $param
      */
-    public function findAddressByRes($param=array(),$f='*'){
+    public function findAddressByRes($param=array(),$f='*',$limit=1){
     	
     	$this->db->select($f);
     	if(!empty($param['uid'])) {
     		$this->db->where('uid',$param['uid']);
     	}
     	$this->db->order_by('is_default','desc'); // 2为默认  1为非默认
+    	$this->db->limit($limit);
     	return $this->db->get($this->table);
     }
 
