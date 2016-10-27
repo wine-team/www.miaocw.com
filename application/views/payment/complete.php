bc6c783443a7deecc785da40be6b83a2a22a677d
柯上将 <604942578@qq.com>
2016-10-28 06:07:15 +0800

    Add files via upload

--- application/views/payment/complete.php
+++ application/views/payment/complete.php
@@ -2,7 +2,7 @@
 <div class="w9 payment" id="content"> 
 	<div class="bgwd over order-creat">
 		<div class="rel">
-		    <?php if($mainOrder->status==2):?>
+		    <?php if($mainOrder->pay_status > 1):?>
 				<p class="f green l_icon iconfont">&#xe60f;</p>
 				<h1 class="f18 yahei c3">支付成功！我们马上安排给你发货，请耐心等待！</h1>
 			<?php else:?>
@@ -12,7 +12,7 @@
 		</div>
 		<p class="lh18">&nbsp;</p>
 		<p>订单编号：
-			<b><?php echo $mainOrder->order_main_sn;?></b>
+			<b><?php echo $mainOrder->pay_id;?></b>
 			<?php $deliver = json_decode($orderProduct[0]->delivery_address);?>
 			<em class="pl30">收货信息：<?php echo $deliver->receiver_name;?>，手机号码</em><?php echo $deliver->tel;?>，<?php echo $deliver->detailed;?>
 		</p>
@@ -23,7 +23,7 @@
 			<?php foreach ($orderProduct as $val):?>
 			<tr> 
 				<td width="350"> 
-					<a href="<?php echo site_url('goods/detail?goods_id='.$val->goods_id);?>" target="_blank">
+					<a href="<?php echo site_url('goods/detail/'.$val->goods_id);?>" target="_blank">
 						<?php echo $val->goods_name;?> <b class="red pl10"><?php echo $val->attr_value;?></b>
 					</a> 
 	  			</td>
