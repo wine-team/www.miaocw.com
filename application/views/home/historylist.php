361a04557eb7ce62a46fd9f8af098a0a8ad4aff8
604942578 <604942578@qq.com>
2016-12-03 20:55:03 +0800

    两性频道

--- application/views/home/historylist.php
+++ application/views/home/historylist.php
@@ -1,8 +1,13 @@
 <?php if (count($history)>0 && !empty($goods) ) :?>
 <?php foreach ($goods->result() as $key=>$item):?>
-<a title="<?php echo $item->goods_name;?>" class="hs_ra" target="_blank" href="<?php echo site_url('goods/detail/'.$item->goods_id)?>">
+<a title="<?php echo $item->goods_name;?>" class="hs_ra" target="_blank" href="<?php echo site_url('goods/detail/'.$item->goods_id);?>">
 	<img width="70" height="70" src="<?php echo $this->config->show_image_thumb_url('mall',strstr($item->goods_img,'|',true));?>">
-	<p>¥<?php echo $item->promote_price;?></p>
+	<?php if( !empty($item->promote_price) && !empty($item->promote_start_date) && !empty($item->promote_end_date) && ($item->promote_start_date<=time()) && ($item->promote_end_date>=time())):?>
+		<?php $total_price = $item->promote_price;?>
+	<?php else:?>
+		<?php $total_price = $item->shop_price;?>
+	<?php endif;?>
+	<p>¥<?php echo $total_price;?></p>
 </a>
 <?php endforeach;?>
 <?php else:?>
