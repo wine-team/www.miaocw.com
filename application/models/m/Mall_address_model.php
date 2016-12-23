c0610df6fef66b3889c3155e59207924b15cda90
604942578 <604942578@qq.com>
2016-12-23 22:02:06 +0800

    订单

---
+++ application/models/m/Mall_address_model.php
@@ -0,0 +1,38 @@
+ <?php
+class Mall_address_model extends CI_Model
+{
+    private $table   = 'mall_address';
+    
+     /**
+     * 发现地址
+     * @param unknown $param
+     */
+    public function findAddressByRes($param=array(),$f='*'){
+    	
+    	$this->db->select($f);
+    	if(!empty($param['uid'])) {
+    		$this->db->where('uid',$param['uid']);
+    	}
+    	$this->db->order_by('is_default','desc'); // 2为默认  1为非默认
+    	return $this->db->get($this->table);
+    }
+
+     /**
+     * 插入
+     * @param unknown $param
+     */
+    public function insert($param) {
+    	
+       $this->db->insert($this->table,$param);
+       return $this->db->insert_id();
+    }
+    
+     /**
+     * delete
+     */
+    public function delete($address_id) {
+    	
+    	$this->db->where('address_id',$address_id);
+    	return $this->db->delete($this->table);
+    }
+}
