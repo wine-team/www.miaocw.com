c81d76009b8453e0de5cbbd5e649fa7764b3beab
604942578 <604942578@qq.com>
2016-12-24 17:37:09 +0800

    地址

--- application/controllers/m/Ucenter.php
+++ application/controllers/m/Ucenter.php
@@ -55,6 +55,11 @@ class Ucenter extends MW_Controller {
 		if (empty($this->d['address_id'])) {
 			$this->jsonMessage('请传地址ID');
 		}
+		$status = $this->mall_address->editAddress($this->d);
+		if ($status) {
+			$this->jsonMessage('','修改成功');
+		}
+		$this->jsonMessage('修改失败');
 	}
 	
 	 /**
@@ -62,7 +67,53 @@ class Ucenter extends MW_Controller {
 	 */
 	public function insertAddress() {
 		
-		
+		if (empty($this->d['uid'])) {
+			$this->jsonMessage('请传用户UID');
+		}
+		if (empty($this->d['province_id'])) {
+			$this->jsonMessage('请传省份ID');
+		}
+		if (empty($this->d['province_name'])) {
+			$this->jsonMessage('请传省份');
+		}
+		if (empty($this->d['city_id'])) {
+			$this->jsonMessage('请传城市ID');
+		}
+		if (empty($this->d['city_name'])) {
+			$this->jsonMessage('请传城市');
+		}
+		if (empty($this->d['district_id'])) {
+			$this->jsonMessage('请传区ID');
+		}
+		if (empty($this->d['district_name'])) {
+			$this->jsonMessage('请传区');
+		}
+		if (empty($this->d['detailed'])) {
+			$this->jsonMessage('请传详细地址');
+		}
+		if (empty($this->d['code'])) {
+			$this->jsonMessage('请传邮政编码');
+		}
+		if (strlen($this->d['code']) !=6 ) {
+			$this->jsonMessage('请传正确的邮政编码');
+		}
+		if (empty($this->d['receiver_name'])) {
+			$this->jsonMessage('请传收件人姓名');
+		}
+		if (empty($this->d['tel'])) {
+			$this->jsonMessage('请传联系电话');
+		}
+		if (!valid_mobile($this->d['tel'])) {
+			$this->jsonMessage('请填正确联系电话');
+		}
+		if (empty($this->d['is_default'])) {
+			$this->jsonMessage('请传是否默认');
+		}
+		$status = $this->mall_address->insert($this->d);
+		if ($status) {
+			$this->jsonMessage('','添加成功');
+		}
+		$this->jsonMessage('添加失败');
 	}
 	
 }
--- application/models/m/Mall_address_model.php
+++ application/models/m/Mall_address_model.php
@@ -40,4 +40,52 @@ class Mall_address_model extends CI_Model
     	}
     	return $this->db->delete($this->table);
     }
+    
+     /**
+     * 编辑
+     * @param unknown $param
+     */
+    public function editAddress($param) {
+    	
+    	if (!empty($param['province_id'])) {
+    		$data['province_id'] = $param['province_id'];
+    	}
+    	if (!empty($param['province_name'])) {
+    		$data['province_name'] = $param['province_name'];
+    	}
+    	if (!empty($param['city_id'])) {
+    		$data['city_id'] = $param['city_id'];
+    	}
+    	if (!empty($param['city_name'])) {
+    		$data['city_name'] = $param['city_name'];
+    	}
+    	if (!empty($param['district_id'])) {
+    		$data['district_id'] = $param['district_id'];
+    	}
+    	if (!empty($param['district_name'])) {
+    		$data['district_name'] = $param['district_name'];
+    	}
+    	if (!empty($param['detailed'])) {
+    		$data['detailed'] = $param['detailed'];
+    	}
+    	if (!empty($param['code'])) {
+    		$data['code'] = $param['code'];
+    	}
+    	if (!empty($param['receiver_name'])) {
+    		$data['receiver_name'] = $param['receiver_name'];
+    	}
+    	if (!empty($param['tel'])) {
+    		$data['tel'] = $param['tel'];
+    	}
+    	if (!empty($param['is_default'])) {
+    		$data['is_default'] = $param['is_default'];
+    	}
+    	if (!empty($param['address_id'])) {
+    		$this->db->where('address_id',$param['address_id']);
+    	}
+    	if (!empty($param['uid'])) {
+    		$this->db->where('uid',$param['uid']);
+    	}
+    	return $this->db->update($this->table,$data);
+    }
 }
